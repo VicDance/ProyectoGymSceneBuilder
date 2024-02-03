0504dcbc8e30b9898e2ae60ff4f85a2392c47105
Vicky <mvictoria_29397@hotmail.es>
2024-02-03 12:49:30 +0100

    add label to cart

---
+++ src/main/java/org/proyecto/proyectogymscenebuilder/controller/CarritoController.java
@@ -0,0 +1,35 @@
+package org.proyecto.proyectogymscenebuilder.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static org.proyecto.proyectogymscenebuilder.controller.UserController.logged;
+import static org.proyecto.proyectogymscenebuilder.controller.UserController.username;
+
+public class CarritoController implements Initializable {
+    @FXML
+    Label carritoUsername;
+
+    @FXML
+    Button añadirCestaId;
+
+    @FXML
+    Label labelCesta;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if (logged) {
+            carritoUsername.setText(carritoUsername.getText() + " " + username);
+        }
+    }
+
+    @FXML
+    public void addToCart() {
+        labelCesta.setText("Añadido al carrito");
+    }
+}
